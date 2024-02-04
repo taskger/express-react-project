1784c63211dd5ac446c4d628a2013f5d2127afb6
taskger <moswit7@gmail.com>
2024-02-05 02:33:53 +0700

    import2

--- client/src/components/Admin/Course/course.js
+++ client/src/components/Admin/Course/course.js
@@ -4,11 +4,22 @@ import "./course.css";
 import { useState } from "react";
 import * as XLSX from "xlsx";
 
-const [data, setData] = useState([]);
-const handleFileUpload = (e) => {}
-
 const Course = () => {
-  return (
+  const [data, setData] = useState([]);
+  const handleFileUpload = (e) => {
+  const reader = new FileReader();
+  reader.readAsBinaryString(e.target.files[0]);
+  reader.onload = (e) => {
+    const data = e.target.result;
+      const workbook = XLSX.read(data, { type: "binary" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      setData(parsedData);
+    };
+  }
+
+return (
     <div>
       <Navbar/>
       <h1>หน้านำเข้าหลักสูตรของ ผู้จัดตาราง</h1>
@@ -29,8 +40,28 @@ const Course = () => {
               <input class="form-control" accept=".xlsx, .xls" type="file" onChange={handleFileUpload} id="formFile"/>
             </div>       
           </div>
+          {data.length > 0 && (
+        <table className="table">
+          <thead>
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <tr key={index}>
+                {Object.values(row).map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
         </div>
-
+       
     </div>
   );
 };
