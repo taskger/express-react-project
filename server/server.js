4234e3d2266b2e7015c8d926f46844ec37f266af
imarayasak <arayasak.u@ku.th>
2024-03-07 21:22:34 +0700

    login

--- server/server.js
+++ server/server.js
@@ -1,14 +1,66 @@
-const express = require('express')
-const mysql = require('mysql');
-const app = express()
+var express = require('express');
+var mysql = require('mysql');
+var session = require('express-session');
+var bodyParser = require ('body-parser');
+var path = require('path');
+var app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // จำนวนรอบในการผสม
+
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true 
+}));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 app.use(express.json());
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
-    database: 'se',
+    password: 'root',
+    database: 'register'
 })
+
+
+
+app.post('/auth', async (request, response) => {
+    const { username, password } = request.body;
+    console.log("Received username:", username);
+    console.log("Received password:", password);
+    if (username && password) {
+        try {
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+            connection.query("SELECT * FROM idAdmin WHERE username = ?", [username], async (err, results, fields) => {
+                if (err) {
+                    console.log("Error executing query:", err);
+                    return response.status(500).send('Internal Server Error');
+                }
+                if (results.length > 0) {
+                    const match = await bcrypt.compare(password, results[0].password);
+                    if (match) {
+                        request.session.loggedin = true;
+                        request.session.username = username;
+                    } else {
+                        return response.status(403).send('Invalid username or password');
+                    }
+                } else {
+                    return response.status(403).send('Invalid username or password');
+                }
+            });
+        } catch (error) {
+            console.log("Error hashing password:", error);
+            return response.status(500).send('Internal Server Error');
+        }
+    } else {
+        return response.status(400).send('Please enter username and password');
+    }
+});
 
 connection.connect((err) => {
     if (err) {
@@ -67,4 +119,7 @@ app.get("/readcourse/single/:catagory", async (req,res) => {
         return res.status(500).send();
     }
 })
-app.listen(5000, () => { console.log("Server started on port 5000")})
+
+app.listen(3036, () => { 
+    console.log("Server started on port 3306");
+});
