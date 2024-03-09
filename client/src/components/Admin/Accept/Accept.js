34f8c30d544ceee781ab8cbf76f299307c3f6bc0
HadezZa <im_arayasak@hotmail.com>
2024-03-09 21:24:47 +0700

    accept

--- client/src/components/Admin/Accept/Accept.js
+++ client/src/components/Admin/Accept/Accept.js
@@ -1,37 +1,56 @@
-import React, { Profiler } from 'react';
-import './Accept.css'
-import { useState } from 'react';
-import { gapi } from "gapi-script";
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import './Accept.css';
 
-function Accept () {
-    const {profile,setProfile} = useState(0)
-    return(
+function Accept() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        Axios.get(`http://localhost:3000/accept/${0}`)
+            .then(response => {
+                setData(response.data.results); // Assuming the response contains the data you need
+                console.log(response.data); // Log the response data
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+    return (
         <div className='box-accept'>
-            <div className='in-accept'>
-                <div className='img-accept'>
-                    <img className='logo-accept'  alt='sdsd'/>
+            {data ? (
+                <div className='in-accept'>
+                    {data.map(item => (
+                        <div key={item.id} className='item-accept'>
+                            <div className='img-accept'>
+                                <img className='logo-accept' src={item.img} alt='sdsd' />
+                            </div>
+                            <div className='kit-accept'/>
+                            <div className='name-accept'>
+                                <h1 className='first-font-accept'>{item.name}</h1>
+                                <h1 className='sec-font-accept'>{item.surname}</h1>
+                            </div>
+                            <div className='kit-accept'/>
+                            <div className='email-accept'>
+                                <h1 className='three-font-accept'>{item.email}</h1>
+                            </div>
+                            <div className='kit-accept'/>
+                            <div className='button-accept'>
+                                <div>
+                                    <button className='yan-accpet'>ยืนยัน</button>
+                                </div>
+                                <div>
+                                    <button className='yox-accpet'>ยกเลิก</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div className='kit-accept'/>
-                <div className='name-accept'>
-                    <h1 className='first-font-accept'>  </h1>
-                    <h1 className='sec-font-accept'> </h1>
-                </div>
-                <div className='kit-accept'/>
-                <div className='email-accept'>
-                    <h1 className='three-font-accept'> </h1>
-                </div>
-                <div className='kit-accept'/>
-                <div className='button-accept'>
-                    <div>
-                        <button className='yan-accpet'>ยืนยัน</button>
-                    </div>
-                    <div>
-                        <button className='yox-accpet'>ยกเลิก</button>
-                    </div>
-                </div>
-            </div>
-        </div>  
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
     );
-};
+}
 
 export default Accept;
--- server/server.js
+++ server/server.js
@@ -25,7 +25,8 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'se'
+    database: 'se',
+    port: 3307
 })
 
 app.post('/check', async (request, response) => {
@@ -93,6 +94,23 @@ app.post("/google", async (req, res) =>{
         return res.status(500).send();
     }
 })
+
+app.get("/accept/:status", async (req,res) => {
+    const { status } = req.params;   
+
+    try {
+        connection.query("SELECT * FROM idUser WHERE status = ? ", [status], (err, results, fields) => {
+            if (err) {
+                console.log(err);
+                return res.status(400).send();
+            }
+            return res.status(200).json({ results });
+        });
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send();
+    }
+});
 
 app.post("/user/addcourse/createlecture", async (req, res) => {
     const { 
