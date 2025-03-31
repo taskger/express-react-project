db6987c6ceec5549447a4aaec6f004e6a0c051a7
taskger <moswit7@gmail.com>
2025-03-31 13:44:54 +0700

    edit cors

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-    origin: ['https://projectschedule.vercel.app'],
+    origin: ['http://projectschedule.vercel.app','https://projectschedule.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
