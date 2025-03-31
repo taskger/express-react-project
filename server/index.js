be3a789c5b074ec9401d57ccf328a9f0d4b39a2d
taskger <moswit7@gmail.com>
2025-03-31 15:18:02 +0700

    add localhost in cors server

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const cors = require('cors');
 
 
 app.use(cors({
-    origin: ['http://projectschedule.vercel.app','https://projectschedule.vercel.app'],
+    origin: ['http://projectschedule.vercel.app','https://projectschedule.vercel.app','http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
